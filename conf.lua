1d8d9e11524b9b27899274abeeebf451fea104cb
Asmageddon <Asmageddon@gmail.com>
2016-06-26 21:40:16 +0200

    Added a conf file, enabled AA

--- conf.lua
+++ conf.lua
@@ -1,1 +1,15 @@
 -- Configurations ahead
+function love.conf(t)
+    t.version = "0.10.1"
+    t.window.msaa = 8
+    t.console = true
+
+    t.window.title = "Newtonian Conundrums"
+    
+    t.window.width = 1024
+    t.window.height = 768
+    t.window.borderless = false
+    t.window.resizable = true
+
+    t.window.vsync = false
+end
