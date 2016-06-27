080913f24bbadc55533ef0a6cae6a5de6441f521
Jonatan H Sundqvist <jonatanhsundqvist@gmail.com>
2016-06-27 14:30:53 +0200

    Fonts and stuff

--- main.lua
+++ main.lua
@@ -2,79 +2,81 @@
 -- Jonatan H Sundqvist
 -- June 25 2016
 
-require "src.love_utils"
-require "src.utils"
+require 'src.love_utils'
+require 'src.utils'
 
-class = require "src.lib.30log-llama"
+class = require 'src.lib.30log-llama'
 
 local Object = class()
 
 function Object:__init(state, pos, shape, density, body_type)
-    self._state = state -- Cache for later
+  self._state = state -- Cache for later
 
-    self.body    = love.physics.newBody(state.world, pos.x, pos.y, body_type) -- Body type is either 'dynamic', 'static' or 'kinematic'
-    self.shape   = shape
-    self.fixture = love.physics.newFixture(self.body, self.shape, density)
-    self.color = {50, 50, 50}
+  self.body    = love.physics.newBody(state.world, pos.x, pos.y, body_type) -- Body type is either 'dynamic', 'static' or 'kinematic'
+  self.shape   = shape
+  self.fixture = love.physics.newFixture(self.body, self.shape, density)
+  self.color = {50, 50, 50}
 end
 
 Object:property(
-    "x",
-    function(self) return self.body:getX() end,
-    function(self, v) return self.body:setX(v) end
+  'x',
+  function(self) return self.body:getX() end,
+  function(self, v) return self.body:setX(v) end
 )
 
 Object:property(
-    "y",
-    function(self) return self.body:getY() end,
-    function(self, v) return self.body:setY(v) end
+  'y',
+  function(self) return self.body:getY() end,
+  function(self, v) return self.body:setY(v) end
 )
 
 function Object:render(options)
-    love.graphics.setColor(unpack(self.color))
-    local shape_type = self.shape:getType()
-    if shape_type == "circle" then
-        love.graphics.circle('fill', self.x, self.y, self.shape:getRadius())
-    elseif shape_type == "polygon" then
-        love.graphics.polygon('fill', self.body:getWorldPoints(self.shape:getPoints()))
-    end
+  love.graphics.setColor(unpack(self.color))
+  local shape_type = self.shape:getType()
+  if shape_type == 'circle' then
+    love.graphics.circle('fill', self.x, self.y, self.shape:getRadius())
+  elseif shape_type == 'polygon' then
+    love.graphics.polygon('fill', self.body:getWorldPoints(self.shape:getPoints()))
+  end
 end
 
 
 function love.load()
-    assets = {}
-    assets.fonts = {}
-    assets.fonts.alameda = love.graphics.newFont('assets/fonts/alameda/alameda.ttf', 30)
-    assets.fonts.kust    = love.graphics.newFont('assets/fonts/Kust_Free_Brush_Font/kust.otf', 30)
-    assets.fonts.bigKust = love.graphics.newFont('assets/fonts/Kust_Free_Brush_Font/kust.otf', 136)
+  assets = {}
+  assets.fonts = {}
+  assets.fonts.alameda  = love.graphics.newFont('assets/fonts/alameda/alameda.ttf', 30)
+  assets.fonts.elixia   = love.graphics.newFont('assets/fonts/elixia.ttf',   30)
+  assets.fonts.aclonica = love.graphics.newFont('assets/fonts/aclonica.ttf', 30)
+  assets.fonts.kust     = love.graphics.newFont('assets/fonts/Kust_Free_Brush_Font/kust.otf', 30)
+  assets.fonts.bigKust  = love.graphics.newFont('assets/fonts/Kust_Free_Brush_Font/kust.otf', 136)
 
-    state = {}
-    state.mode = "interactive"
-    state.running = true
-    love.physics.setMeter(64) --the height of a meter our worlds will be 64px
-    state.world = love.physics.newWorld(0, 9.81*64, true) --create a world for the bodies to exist in with horizontal gravity of 0 and vertical gravity of 9.81
+  state = {}
+  state.mode = 'interactive'
+  state.running = true
+  love.physics.setMeter(64) --the height of a meter our worlds will be 64px
+  state.world = love.physics.newWorld(0, 9.81*64, true) --create a world for the bodies to exist in with horizontal gravity of 0 and vertical gravity of 9.81
 
-    state.world:setCallbacks(callbacks.beginContact, callbacks.endContact, callbacks.preSolve, callbacks.postSolve)
+  state.world:setCallbacks(callbacks.beginContact, callbacks.endContact, callbacks.preSolve, callbacks.postSolve)
 
-    -- table to hold all our physical objects
-    local w, h = love.graphics.getDimensions()
+  -- table to hold all our physical objects
+  local w, h = love.graphics.getDimensions()
 
-    state.objects = {}
-    state.objects.ground = Object(state, { x=w/2, y=h-50/2 },       love.physics.newRectangleShape(w, 50),  3, 'static')
-    state.objects.ground.color = {25, 190, 105}
-    state.objects.ball = Object(state, { x=w/2, y=h/2},           love.physics.newCircleShape(20),         3, 'dynamic')
-    state.objects.ball.fixture:setRestitution(0.8) -- Let the ball bounce
-    state.objects.ball.color = {193, 47, 14}
-    state.objects.block1 = Object(state, { x=w/2, y=h-100/2 },      love.physics.newRectangleShape(50, 100), 5, 'dynamic')
-    state.objects.block2 = Object(state, { x=w/2, y=h-100/2-50/2 }, love.physics.newRectangleShape(100, 50), 5, 'dynamic')
+  state.objects = {}
+  state.objects.ground = Object(state, { x=w/2, y=h-50/2 },       love.physics.newRectangleShape(w, 50),  3, 'static')
+  state.objects.ground.color = {25, 190, 105}
+  state.objects.ball = Object(state, { x=w/2, y=h/2},           love.physics.newCircleShape(20),         3, 'dynamic')
+  state.objects.ball.fixture:setRestitution(0.8) -- Let the ball bounce
+  state.objects.ball.color = {193, 47, 14}
+  state.objects.block1 = Object(state, { x=w/2, y=h-100/2 },      love.physics.newRectangleShape(50, 100), 5, 'dynamic')
+  state.objects.block2 = Object(state, { x=w/2, y=h-100/2-50/2 }, love.physics.newRectangleShape(100, 50), 5, 'dynamic')
 
-    for i=1,8 do
-        local new_object = Object(state, { x=(i*60)+(w/2)-600, y=h-140/2 }, love.physics.newRectangleShape(24, 140), 8, 'dynamic')
-        table.insert(state.objects, new_object)
-    end
+  for i=1,8 do
+    local new_object = Object(state, { x=(i*60)+(w/2)-600, y=h-140/2 }, love.physics.newRectangleShape(24, 140), 8, 'dynamic')
+    table.insert(state.objects, new_object)
+  end
 
-    love.graphics.setBackgroundColor(104, 136, 248) -- Set the background color to a nice blue
-    love.graphics.setFont(assets.fonts.kust)
+  love.graphics.setBackgroundColor(104, 136, 248) -- Set the background color to a nice blue
+  love.graphics.setFont(assets.fonts.kust)
 end
 
 
@@ -108,13 +110,14 @@ function love.update(dt)
 
     --here we are going to create some keyboard events
     if love.keyboard.isDown('right') then --press the right arrow key to push the ball to the right
-        state.objects.ball.body:applyForce(400, 0)
+      state.objects.ball.body:applyForce(400, 0)
     elseif love.keyboard.isDown('left') then --press the left arrow key to push the ball to the left
-        state.objects.ball.body:applyForce(-400, 0)
+      state.objects.ball.body:applyForce(-400, 0)
     elseif love.keyboard.isDown('up') then --press the up arrow key to set the ball in the air
-        state.objects.ball.body:setPosition(love.graphics.getWidth() / 2, love.graphics.getHeight() / 2)
+      state.objects.ball.body:setPosition(love.graphics.getWidth() / 2, love.graphics.getHeight() / 2)
+      state.objects.ball.body:setLinearVelocity(0, 0)
     elseif love.keyboard.isDown('space') then --press the left arrow key to push the ball to the left
-        state.objects.ball.body:applyForce(0, -400)
+      state.objects.ball.body:applyForce(0, -400)
     end
 
     state.mode = love.keyboard.isDown('tab') and 'editor' or 'interactive'
@@ -145,7 +148,7 @@ function love.draw()
   local w, h   = love.graphics.getDimensions()
 
   for _, obj in pairs(state.objects) do
-      obj:render()
+    obj:render()
   end
 
   -- GUI Overlay
